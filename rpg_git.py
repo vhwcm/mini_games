7f3db4f41c19a2c511aece29773f9cfca48e5beb
vikhwcm@note-viktor <vikhwcm@note-viktor>
2024-05-10 21:26:24 -0300

    rpg

---
+++ rpg_git.py
@@ -0,0 +1,55 @@
+import google.generativeai as genai
+
+API_KEY = 'YOUR API_KEY'
+
+genai.configure(api_key=API_KEY)
+
+generation_config = {
+  "candidate_count": 1,
+  "temperature": 0.
+}
+
+safety_settings={
+    'HATE': 'BLOCK_NONE',
+    'HARASSMENT': 'BLOCK_NONE',
+    'SEXUAL' : 'BLOCK_NONE',
+    'DANGEROUS' : 'BLOCK_NONE'
+    }
+system_instruction =  "Estamos em um jogo de rpg.\n\ninicialmente eu poderei escolher um nome para o meu reino, meu  nome e espécie. gostaria que o rpg fosse de estratégia, contento quizes, perguntas e desiões de estratégia. Eu serei o rei, e tomarei desições economicas, religiosas e militares. Quero que no INICIO de cada resposta tenha um menu básico indicando o estatus do meu reino de: Felicidade[0-100%], Poder militar[0 - 100000], atual religião do reino[no jogo existira religioes que não exitem na vida real, como por exemplo uma religiao de orcs.], Dinheiro[0 - 100000],\nnome do meu reino. minhas deçisoes afetaram os status do meu reino. Durante o jogo poderei fazer aliados e inimigos mortais, sendo que eles podem me atacar ou me proteger. O jogo funcionará como se voce fosse um servo do reino que me explicasse o que está acontecendo e me pedisse para escolher o que fazer. Eu posso fazer perguntas para vc para vc me detalhar algo. Sempre que um novo império entrar em contato, mostra os status de tal império como a barra de menu básica. Normalmente me de opções de numeradas para eu fazer uma decisão. no inicio voce sempre receberá um contexto na forma cronológica, só manter o contexto."
+system_instruction_h = "eu quero que com os dados que eu te dei vc guarde as informações, sem tópicos e em único paragrafo, como se fosse um livro com todas as informações que eu te dei, sem ser em formato de jogo, como se vc fosse um narrador oniciente.Guarde os status do reino. Ignora o que lhe for dado em que esteja numerado(pois seria as opçoes do jogador, mas ele não executara todas) Sempre que eu te mandar algo, escreva a continução da sua respostada dada anteriormente, com os novos dados. Escreva no máximo 3 linhas e não coloque nada que não coloque caracteres especias.LEMBRE=SE DE ESCREVER OS STATUS DO REINO"
+
+nome_avetura = input("Qual o nome da aventura? ")
+model_h = genai.GenerativeModel(model_name='gemini-1.5-pro-latest',generation_config=generation_config,system_instruction=system_instruction_h, safety_settings=safety_settings,)
+chat_history = model_h.start_chat(history=[])
+model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest',generation_config=generation_config,system_instruction=system_instruction, safety_settings=safety_settings,)
+chat = model.start_chat(history=[])
+nome = input("qual seu é nome? ")
+reino = input("qual é o nome do reino? ")
+raça = input("qual é a raça do seu reino? ")
+con = input("Já possui um reino?")
+i = 1
+while(i == 1):
+  if (con == "não"):
+      pergunta = "Quero Criar um reino chamado "+reino+" onde eu serei o rei "+nome+" e a raça do reino será "+raça+"."
+      i = 2
+  elif (con == "sim"):
+      pergunta = "Eu sou o rei de "+reino+" e meu nome é "+nome+" da "+raça+", gostaria de saber como está meu reino."
+      i = 2
+  else:
+    print("Responda sim ou não")
+with open(f'{nome_avetura}.txt', 'a+') as f:
+  f.seek(0)
+  conteudo = f.read()
+  while(pergunta != "end"):               
+    mensagem = conteudo + "\n\n" + pergunta
+    chat.send_message(mensagem)
+    print("\n",chat.last.text,"\n")         
+    continuacao = chat.last.text
+    chat_history.send_message(continuacao+"guarde os status do reino, como religiao, poder militar, dinheiro e felicidade.")
+    f.write(chat_history.last.text)
+    f.seek(0)   
+    conteudo = f.read()
+    pergunta = input(f'PROMPT: ')
+f.close()
+print("\nFim do jogo\n")
+
